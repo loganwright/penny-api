dc0e3516c6fcd98bf5a4a16e57777ed5e36b3b78
Logan Wright <logan.william.wright@gmail.com>
2018-05-04 15:36:38 -0500

    all systems working

--- Sources/App/GitHubIntegration/GitHubWebHookRoutes.swift
+++ Sources/App/GitHubIntegration/GitHubWebHookRoutes.swift
@@ -1,9 +1,12 @@
 import Vapor
 
+let GITHUB_GENERAL_WEBHOOK_SECRET = Environment.get("GITHUB_GENERAL_WEBHOOK_SECRET")!
+let GITHUB_VALIDATION_WEBHOOK_SECRET = Environment.get("GITHUB_VALIDATION_WEBHOOK_SECRET")!
+
 func githubapi(_ router: Router) throws {
     // GitHub General Listening
     router.post("gh-webhook") { req -> Future<HTTPStatus> in
-        let hook = try req.webhook(secret: "foo-bar")
+        let hook = try req.webhook(secret: GITHUB_GENERAL_WEBHOOK_SECRET)
         let runner = WebHookRunner(req, githubToken: PENNY_GITHUB_TOKEN)
         // Right now, only support Pull Requests, will expand
         return hook.flatMap(to: HTTPStatus.self, runner.handlePullRequest)
@@ -11,7 +14,7 @@ func githubapi(_ router: Router) throws {
 
     // GitHub Account Linking
     router.post("gh-validation-hook") { req -> Future<HTTPStatus> in
-        let hook = try req.webhook(secret: nil)
+        let hook = try req.webhook(secret: GITHUB_VALIDATION_WEBHOOK_SECRET)
         let validator = GitHubLinkValidator(req, githubToken: PENNY_GITHUB_TOKEN)
         return hook.flatMap(to: HTTPStatus.self, validator.handle)
     }
