f9dec2d378dd9709c4cd1946d881bae135fb405a
logan <logan.william.wright@gmail.com>
2019-03-21 22:24:25 +0100

    update to 3.3

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.3.0"),
         .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
     ],
     targets: [
