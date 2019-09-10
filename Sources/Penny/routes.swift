5df7ae702d22bdef27b36f39fbaa6025f722ecd4
Logan Wright <logan.william.wright@gmail.com>
2019-09-10 20:12:03 +0200

    lower

--- Sources/Penny/routes.swift
+++ Sources/Penny/routes.swift
@@ -4,7 +4,7 @@ import Vapor
 public func routes(_ router: Router) throws {
     // I always keep a status check
     router.get("status") { req in
-        return "Alive and well: \(Date())"
+        return "alive and well: \(Date())"
     }
 
     // Penny Endpoints
